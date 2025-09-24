a5257dbfe15023342d084ac2bc17d7c8a6d2fbaa
kkatushaa <159055746+kkatushaa@users.noreply.github.com>
2025-09-24 16:57:42 +0300

    Update task.js

---
+++ dom/reveal/task.js
@@ -0,0 +1,14 @@
+document.addEventListener('scroll', () => {
+    const reveals = document.querySelectorAll('.reveal');
+
+    reveals.forEach(reveal => {
+        const rect = reveal.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (rect.top < windowHeight && rect.bottom > 0) {
+            reveal.classList.add('reveal_active');
+        } else {
+            reveal.classList.remove('reveal_active');
+        }
+    });
+});
